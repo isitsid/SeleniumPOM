7550e6883a0835cfbf4f0f5293f4eb7f773ca0de
Siddartha <siddartha@admins-MacBook-Pro-81.local>
2021-06-07 23:34:37 +0530

    Initial commit

---
+++ Src/PageObject/Locators.py
@@ -0,0 +1,6 @@
+class Locator(object):
+    # Google Search Page
+    search_text = "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"
+    submit = "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]"
+    # //input[@name='Google Search']"
+    logo = "/html/body/div[1]/div[2]/div/img"
